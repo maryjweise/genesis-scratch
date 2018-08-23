a5b3c7b5facbba0d3e1f7b875e98fae6046de761
maryjweise <maryjweise@gmail.com>
2018-08-23 13:14:08 -0700

    update Theme URI with github link

--- functions.php
+++ functions.php
@@ -26,4 +26,13 @@ add_action( 'genesis_setup', 'scratch_setup' );
  */
 function scratch_setup(){
     
+    // Define theme constants
+    define( 'CHILD_THEME_NAME', 'Scratch' );
+    define( 'CHILD_THEME_URL', 'https://github.com/maryjweise/genesis-scratch' );
+    define( 'CHILD_THEME_VERSION', '1.0.0' );
+    
+    add_theme_support() {
+        
+    }
+    
 }
